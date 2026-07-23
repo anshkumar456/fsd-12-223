a563229071d21e52503cf90059d8f4a06a8b43db
anshkumar456 <v61710603@gmail.com>
2026-07-23 11:55:37 +0530

    hello.js

---
+++ lab1/hello.js
@@ -0,0 +1,15 @@
+console.log("ansh")
+
+// write a function that takes two numbers as arguments and returns their sum
+function sum(a, b){
+    return a+b;
+}
+
+const add =( a,b) =>{
+    return a+b;
+};
+const add2 = (a,b) => a+b;
+
+console.log(sum(20,10));
+console.log(add(20,10));
+console.log(add2(20,10));
