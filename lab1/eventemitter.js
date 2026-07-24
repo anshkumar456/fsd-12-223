9f56f9136f184b76ed621ada60a0e7f9be98d21a
anshkumar456 <v61710603@gmail.com>
2026-07-24 14:37:57 +0530

    eventemitter.js

---
+++ lab1/eventemitter.js
@@ -0,0 +1,13 @@
+const login = (name) => {
+    console.log('${name} logged in');
+};
+
+const start = () => {
+    console.log("system starts");
+};
+const working = (name) => {
+    console.log('${name} add items to cart');
+};
+const checkout = (name) => {
+    console.log('${name} logged out');
+}
