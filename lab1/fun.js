ae62eca0434cd526f2f35f68de76a4682e6e0bb7
anshkumar456 <v61710603@gmail.com>
2026-07-24 14:27:13 +0530

    fun

---
+++ lab1/fun.js
@@ -0,0 +1,7 @@
+const towards =(digit)=>{
+    const words = ["zero","one","two","three","four","five","six","seven","eight","nine"];
+    return words [digit];
+}
+console.log(towards(5));
+console.log(towards(3));
+console.log(towards(6));
