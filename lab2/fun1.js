0351de8ed761cb43adf380a3c3c544c40cf8bfba
anshkumar456 <v61710603@gmail.com>
2026-07-31 15:16:17 +0530

    lab1 revise

--- lab2/fun1.js
+++ lab2/fun1.js
@@ -1,18 +1,48 @@
+//const f1 = () => {
+ // console.log("f1");
+//};
+//const f2 = () => {
+  //console.log("f2");
+//};
+//const f3 = () => {
+  //console.log("f3");
+//};
+
+//function main() {
+  //console.log("main");
+  //setTimeout(f1,0);
+  //setTimeout(f2,0);
+  //setTimeout(f1, 1000);
+  //setImmediate(f2);
+  //Process.nextTicket(f3);
+  //console.log("end");
+//}
+//main();
 const f1 = () => {
-    console.log("f1");
+  console.log("f1");
 };
 const f2 = () => {
-    console.log("f2");
+  console.log("f2");
 };
 const f3 = () => {
-    console.log("f3");
+  console.log("f3");
 };
 
 function main() {
-    console.log("main");
-    f1();
-    f2();
-    f3();
-    console.log("end");
+  console.log("main");
+  setTimeout(f1, 1000);
+  setTimeout(f2, 700);
+  process.nextTick(f3);   // ✅ correct API
+  console.log("end");
 }
+
 main();
+
+function main(){
+  console.log("main");
+  setTimeout(f1, 50);
+  setTimeout(f2, 30);
+  new Promise((resolve, reject) => {
+    
+  })
+}
---
+++ lab2/readme.md
@@ -0,0 +1,16 @@
+#EventLoop
+
+Js is synchronous and single threaded bydefault 
+
+## There can be async behaviour 
+- with BrowserAPI- setTimeout,setInterval,setImmediate,nextTick
+- with promises
+- with event handlers 
+a function not execuited immediately but it must be execuited after a while.
+it has some status during the execution .
+at final it may resolve 
+modern java script is divided into two catogery
+first < commonJs(.cjs)> support oops , require
+-priority
+second< moduleJs(.mjs)> follow modern approch , import
+-priority
